893e3447fc17f0d1d9c4365843b50642525bb71f
Thomas <yitunghuang83@gmail.com>
2020-06-27 17:09:06 +0930

    Add QuestionsControl to DocumentControl

--- src/components/DocumentControl.js
+++ src/components/DocumentControl.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
+import QuestionsControl from './QuestionsControl.js';
 
 export default class DocumentControl extends React.Component {
   constructor(props){
     super(props);
-
-    this.updateTitle = this.updateTitle.bind(this);
-    this.updateNumQuestions = this.updateNumQuestions.bind(this);
-    this.updateNumColumns = this.updateNumColumns.bind(this);
-  }
-
-  updateTitle( event ){
-    this.props.updateTitle( event.target.value );
-  }
-
-  updateNumQuestions( event ){
-    this.props.updateNumQuestions( event.target.value );
-  }
-
-  updateNumColumns( event ){
-    this.props.updateNumColumns( event.target.value );
   }
 
   render() {
@@ -30,31 +14,10 @@ export default class DocumentControl extends React.Component {
         </section>
         <section className="sidebar__section">
           <h3 className="sidebar__section__title">Page layout</h3>
-          <div className="sidebar__section__content">
-            <Form.Group controlId="form__doc__title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control type="text"
-                            placeholder="Enter title of document"
-                            defaultValue={ this.props.document.title }
-                            onChange={ this.updateTitle } />
-            </Form.Group>
-
-            <Form.Group controlId="form__doc__num-questions">
-              <Form.Label>Number of questions</Form.Label>
-              <Form.Control type="text"
-                            placeholder="Enter number of questions"
-                            defaultValue={ this.props.document.numQuestions }
-                            onChange={ this.updateNumQuestions } />
-            </Form.Group>
-
-            <Form.Group controlId="form__doc__num-columns">
-              <Form.Label>Number of columns</Form.Label>
-              <Form.Control type="text"
-                            placeholder="Enter number of columns"
-                            defaultValue={ this.props.document.columns }
-                            onChange={ this.updateNumColumns } />
-            </Form.Group>
-          </div>
+          <QuestionsControl document={this.props.document}
+                            updateTitle={ this.props.updateTitle }
+                            updateNumQuestions={ this.props.updateNumQuestions }
+                            updateNumColumns={ this.props.updateNumColumns }/>
         </section>
       </div>
     );
